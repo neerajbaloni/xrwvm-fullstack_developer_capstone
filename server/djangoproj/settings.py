2f4aea027e7bea76a1a1d34180ac17555a839111
neerajbaloni <neerajbaloni@gmail.com>
2024-10-24 07:38:27 -0400

    lint fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -147,9 +147,10 @@ STATICFILES_DIRS = [
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://neerajbaloni-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    """https://neerajbaloni-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01
+    .proxy.cognitiveclass.ai"""
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://neerajbaloni-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    """https://neerajbaloni-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01
+    .proxy.cognitiveclass.ai"""
 ]
-
